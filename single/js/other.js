2de1c8c5061d529a56fd85612561f36fb8d62b59
JacksonMeng <15738896272@163.com>
2018-07-01 16:50:18 +0800

    Add files via upload

---
+++ single/js/other.js
@@ -0,0 +1,72 @@
+/*** @file Describe the file* 此文件需要全局模板引入* base variables**/
+(function(win) {
+	var flag = 'none';
+	win.slpConfig = {
+		fclickUrl: 'https://sp0.baidu.com/-rU_dTmfKgQFm2e88IuM_a/w.gif',
+		bblFclickUrl: 'https://sp0.baidu.com/5bgWsjip0QIZ8tyhnq/roi',
+		nuomiFclickUrl: 'https://m.nuomi.com/s.gif',
+		slpFclickUrl: 'https://sp0.baidu.com/5bgWsjip0QIZ8tyhnq/w.gif',
+		nFclickUrl: 'https://ada.baidu.com/nclick/index',
+		PARAMS: {
+			searchid: '1277532542',
+			baiduid: '601B21562A0AA147A3E457CC90D834E7:FG=1',
+			tradeid: '6',
+			advsid: '6f0a96ef255e3b41',
+			cmatch: '223',
+			log_version: 'v1',
+			rank: '0',
+			flag: flag,
+			pageid: '85604',
+			page_type: 'main',
+			page_tag: '',
+			cvt_data: 'tel_form_phone',
+			bidword: '北京高压清洗下水道',
+			userid: '22385570'
+		},
+		nParams: {
+			searchid: '1277532542',
+			baiduid: '601B21562A0AA147A3E457CC90D834E7:FG=1',
+			tradeid: '6',
+			advsid: '6f0a96ef255e3b41',
+			cmatch: '223',
+			rank: '0',
+			cvt_data: 'tel_form_phone',
+			page_type: 'main',
+			userid: '22385570'
+		},
+		bblParams: {
+			action: 'slproi',
+			advsid: '6f0a96ef255e3b41',
+			cmatch: '223',
+			rank: parseInt('0') + 1,
+			userid: '22385570',
+			ip: '210.12.230.242'
+		},
+		nuomiParams: {
+			cid: 'fczhitongche',
+			channel_content: '6f0a96ef255e3b41' + '-' + '223' + '-' + '0' + '-' + '' + '-0',
+			type: 10,
+			user_cookie: '',
+			comppage: 'zhineng_detail_local',
+			client_ip: '210.12.230.242',
+			client_type: 'WAP_SMART'
+		},
+		searchid: '1277532542',
+		timeSign: '',
+		query: '高压清洗下水道',
+		WINFO: '',
+		bidword: '北京高压清洗下水道',
+		keywordid: '15406036',
+		rank: '0',
+		cmatch: '223',
+		baiduid: '601B21562A0AA147A3E457CC90D834E7:FG=1',
+		tradeid: '6',
+		userid: '22385570',
+		userid64: 'nWc1rjRdP10',
+		page_type: 'main',
+		page_tag: '',
+		pageid: '85604',
+		bridgeEnterUrl: 'https://p.qiao.baidu.com/Enter.php',
+		bridgeRefreshUrl: 'https://p.qiao.baidu.com/Refresh.php'
+	};
+})(window);
